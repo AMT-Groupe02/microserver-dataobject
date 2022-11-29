49973c2bd49ef552d1a2bf273823f59c2cef5bf3
Luca Coduri <coduri.vitor@gmail.com>
2022-11-29 16:58:57 +0100

    Initial commit

---
+++ src/main/java/com/groupe2/microserverdataobject/MicroserverDataobjectApplication.java
@@ -0,0 +1,13 @@
+package com.groupe2.microserverdataobject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserverDataobjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserverDataobjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupe2/microserverdataobject/MicroserverDataobjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupe2.microserverdataobject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserverDataobjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
