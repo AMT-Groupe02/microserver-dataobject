f5ea48a7d221ecd95b11c1150fc7508f8c59daac
Luca <coduri.vitor@gmail.com>
2022-12-21 15:08:58 +0100

    project rename

--- src/main/java/com/groupe2/microservicedataobject/MicroserviceDataobjectApplication.java
+++ src/main/java/com/groupe2/microservicedataobject/MicroserviceDataobjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MicroserverDataobjectApplication {
+public class MicroserviceDataobjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MicroserverDataobjectApplication.class, args);
+        SpringApplication.run(MicroserviceDataobjectApplication.class, args);
     }
 
 }
